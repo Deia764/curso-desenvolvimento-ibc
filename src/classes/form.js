243948dbe414c0df5522e4f9003701dac859f443
Deia764 <andreia.souza@ibc.gov.br>
2025-10-16 18:39:07 -0300

    Refactor Form class to use IIFE pattern

--- src/classes/form.js
+++ src/classes/form.js
@@ -1,44 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Form = void 0;
-class Form {
-    constructor(nome, email, mensagem) {
-        this.nome = nome;
-        this.email = email;
-        this.mensagem = mensagem;
-        this.id = crypto.randomUUID();
+export var Form = /** @class */ (function () {
+    function Form(nome, email, mensagem) {
+        this.id = new Date().getTime().toString();
         this.nome = nome;
         this.email = email;
         this.mensagem = mensagem;
     }
-    cadastrar() {
-        const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm.push(this);
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
-    static listar() {
-        const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        return listaForm;
-    }
-    static excluir(id) {
-        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm = listaForm.filter((form) => form.id !== id);
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
-    static alterar(formAlterado) {
-        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm = listaForm.map((form) => {
-            if (form.id === formAlterado.id) {
-                return formAlterado;
-            }
-            return form;
-        });
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
-    static buscar(id) {
-        let listaForm = this.listar();
-        let form = listaForm.find(form => form.id === id);
-        return form;
-    }
-}
-exports.Form = Form;
+    Form.prototype.cadastrar = function () {
+        var lista = Form.listar();
+        lista.push(this);
+        localStorage.setItem("listaForm", JSON.stringify(lista));
+    };
+    Form.listar = function () {
+        var dados = localStorage.getItem("listaForm");
+        return dados ? JSON.parse(dados) : [];
+    };
+    Form.buscar = function (id) {
+        return Form.listar().find(function (f) { return f.id === id; });
+    };
+    Form.alterar = function (formAlterado) {
+        var lista = Form.listar();
+        var index = lista.findIndex(function (f) { return f.id === formAlterado.id; });
+        if (index !== -1) {
+            lista[index] = formAlterado;
+            localStorage.setItem("listaForm", JSON.stringify(lista));
+        }
+    };
+    Form.excluir = function (id) {
+        var lista = Form.listar().filter(function (f) { return f.id !== id; });
+        localStorage.setItem("listaForm", JSON.stringify(lista));
+    };
+    return Form;
+}());
