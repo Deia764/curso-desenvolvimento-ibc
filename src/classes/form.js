913828085ef851d6abf633f0a1898f70062d77d7
Deia764 <andreia.souza@ibc.gov.br>
2025-10-16 18:58:37 -0300

    Remove buscar and alterar methods from Form
    Removed buscar and alterar methods from Form class.

--- src/classes/form.js
+++ src/classes/form.js
@@ -14,17 +14,6 @@ export var Form = /** @class */ (function () {
         var dados = localStorage.getItem("listaForm");
         return dados ? JSON.parse(dados) : [];
     };
-    Form.buscar = function (id) {
-        return Form.listar().find(function (f) { return f.id === id; });
-    };
-    Form.alterar = function (formAlterado) {
-        var lista = Form.listar();
-        var index = lista.findIndex(function (f) { return f.id === formAlterado.id; });
-        if (index !== -1) {
-            lista[index] = formAlterado;
-            localStorage.setItem("listaForm", JSON.stringify(lista));
-        }
-    };
     Form.excluir = function (id) {
         var lista = Form.listar().filter(function (f) { return f.id !== id; });
         localStorage.setItem("listaForm", JSON.stringify(lista));
