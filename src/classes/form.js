4a89fde5b83dda4fec9d8ffee73590de308d0193
Deia764 <andreia.souza@ibc.gov.br>
2025-10-16 21:18:28 -0300

    Replace ID generation with crypto.randomUUID

--- src/classes/form.js
+++ src/classes/form.js
@@ -1,24 +1,24 @@
 export class Form {
-    constructor(nome, email, mensagem) {
-        this.nome = nome;
-        this.email = email;
-        this.mensagem = mensagem;
-        this.id = new Date().getTime().toString();
-    }
+  constructor(nome, email, mensagem) {
+    this.id = crypto.randomUUID();
+    this.nome = nome;
+    this.email = email;
+    this.mensagem = mensagem;
+  }
 
-    cadastrar() {
-        const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm.push(this);
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
+  cadastrar() {
+    const listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+    listaForm.push(this);
+    localStorage.setItem("listaForm", JSON.stringify(listaForm));
+  }
 
-    static listar() {
-        return JSON.parse(localStorage.getItem("listaForm") || "[]");
-    }
+  static listar() {
+    return JSON.parse(localStorage.getItem("listaForm") || "[]");
+  }
 
-    static excluir(id) {
-        let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
-        listaForm = listaForm.filter(form => form.id !== id);
-        localStorage.setItem("listaForm", JSON.stringify(listaForm));
-    }
+  static excluir(id) {
+    let listaForm = JSON.parse(localStorage.getItem("listaForm") || "[]");
+    listaForm = listaForm.filter(f => f.id !== id);
+    localStorage.setItem("listaForm", JSON.stringify(listaForm));
+  }
 }
