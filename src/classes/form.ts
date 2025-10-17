b832b0b62382a27ed5127002b099588cfcc6d8e0
Deia764 <andreia.souza@ibc.gov.br>
2025-10-16 21:20:23 -0300

    Refactor Form class to simplify structure
    Removed ID management and localStorage methods from Form class.

--- src/classes/form.ts
+++ src/classes/form.ts
@@ -1,28 +1,12 @@
 export class Form {
-  public id: string;
+  nome: string;
+  email: string;
+  mensagem: string;
 
-  constructor(
-    public nome: string,
-    public email: string,
-    public mensagem: string
-  ) {
-    this.id = new Date().getTime().toString();
-  }
-
-  cadastrar(): void {
-    const listaForm: Form[] = JSON.parse(localStorage.getItem("listaForm") || "[]");
-    listaForm.push(this);
-    localStorage.setItem("listaForm", JSON.stringify(listaForm));
-  }
-
-  static listar(): Form[] {
-    const listaForm: Form[] = JSON.parse(localStorage.getItem("listaForm") || "[]");
-    return listaForm;
-  }
-
-  static excluir(id: string): void {
-    let listaForm: Form[] = JSON.parse(localStorage.getItem("listaForm") || "[]");
-    listaForm = listaForm.filter((form: Form) => form.id !== id);
-    localStorage.setItem("listaForm", JSON.stringify(listaForm));
+  constructor(nome: string, email: string, mensagem: string) {
+    this.nome = nome;
+    this.email = email;
+    this.mensagem = mensagem;
   }
 }
+
